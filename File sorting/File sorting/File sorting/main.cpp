32650fe5c21cfdb1bfccffe351d24776be1765e3
stupa <stupa@DESKTOP-J89NNH6.local>
2024-03-06 18:57:59 +0300

    Переписал первое разбиение, и функцию слияния файлов. Последний элемент fb идет в fc, хотя не должен (видно если посмотреть как разбились fa и fb, и самому слить их в fc и fd), где он это делает понять не могу

--- File sorting/File sorting/File sorting/main.cpp
+++ File sorting/File sorting/File sorting/main.cpp
@@ -32,68 +32,174 @@ bool random_file_gen(const std::string& name, int& lim, int& size) {
 	return true;
 }
 
-bool file_splitting(std::string& name1, std::string& name2, std::string& name3, std::string& name4, int& k) {
-	std::ofstream f1;
-	f1.open(name1);
-	if (!f1) return false;
-	f1.close();
-	std::ifstream file1;
-	file1.open(name1);
+bool file_splitting(std::string& name1, std::string& name2, std::string& name3, std::string& name4) {
+	std::ifstream fa(name1);
+	if (!fa) return -1;
+	std::ifstream fb(name2);
+	if (!fb) return -1;
+	std::ofstream fc(name3);
+	if (!fc) return -1;
+	std::ofstream fd(name4);
+	if (!fd) return -1;
 
-	std::ofstream f2;
-	f2.open(name2);
-	if (!f2) return false;
-	f2.close();
-	std::ifstream file2;
-	file2.open(name2);
-
-	std::ofstream file3;
-	file3.open(name3);
-	if (!file3) return false;
-
-	std::ofstream file4;
-	file4.open(name4);
-	if (!file4) return false;
-
-	int a, b;
-	file1 >> a;
-	file2 >> b;
-	for (int i = 0; i < k/2 + 1; i++) {
-		while (a != INT_MAX && b != INT_MAX) {
-			if (a <= b) {
-				file3 << a << " ";
-				file1 >> a;
-
+	int a, b, c, d;
+	fa >> a >> b;
+	fb >> c >> d;
+	while (fa && fb) {
+		while (a < b && c < d) {
+			if (a < c) {
+				fc << a << " ";
+				a = b;
+				fa >> b;
 			}
-			if (b <= a) {
-				file3 << a << " ";
-				file2 >> b;
-
+			else {
+				fc << c << " ";
+				c = d;
+				fb >> d;
 			}
 		}
-		file1 >> a;
-		file2 >> b;
-		while (a != INT_MAX && b != INT_MAX) {
-			if (a <= b) {
-				file4 << a << " ";
-				file1 >> a;
-
+		if (c >= d) {
+			while (a < b) {
+				if (c < a) {
+					fc << c << " ";
+					c = INT_MAX;
+				}
+				fc << a << " ";
+				a = b;
+				fa >> b;
 			}
-			if (b <= a) {
-				file4 << a << " ";
-				file2 >> b;
-
+			if (c == INT_MAX) {
+				fc << a << " ";
+				a = b;
+				fa >> b;
+				c = d;
+				fb >> d;
+			}
+			else {
+				if (c < a) {
+					fc << c << " " << a << " ";
+					c = d;
+					fb >> d;
+					a = b;
+					fa >> b;
+				}
+				else {
+					fc << a << " " << c << " ";
+					c = d;
+					fb >> d;
+					a = b;
+					fa >> b;
+				}
 			}
 		}
-		file1 >> a;
-		file2 >> b;
+		else {
+			while (c < d) {
+				if (a < c) {
+					fc << a << " ";
+					a = INT_MAX;
+				}
+				fc << c << " ";
+				c = d;
+				fb >> d;
+			}
+			if (a == INT_MAX) {
+				fc << c << " ";
+				c = d;
+				fb >> d;
+				a = b;
+				fa >> b;
+			}
+			else {
+				if (a < c) {
+					fc << a << " " << c << " ";
+					c = d;
+					fb >> d;
+					a = b;
+					fa >> b;
+				}
+				else {
+					fc << c << " " << a << " ";
+					c = d;
+					fb >> d;
+					a = b;
+					fa >> b;
+				}
+			}
+		}
+		fc.swap(fd);
 	}
-	file3.close();
-	file4.close();
-	std::ifstream f4;
-	f4.open(name2);
-	if (f4.peek() == EOF) return true;
-	else return false;	 
+	if (!fa) {
+		while (fb) {
+			while (c < d) {
+				//if (a < c) {
+				//	fc << a << " " << c << " ";
+				//	a = INT_MAX;
+				//	c = d;
+				//	fb >> d;
+				//}
+				fc << c << " ";
+				c = d;
+				fb >> d;
+			}
+			fc << c << " ";
+			c = d;
+			fb >> d;
+			//if (a != INT_MAX) {
+			//	if (a < c) {
+			//		fc << a << " " << c << " ";
+			//	}
+			//	else {
+			//		fc << c << " " << a << " ";
+			//	}
+			//}
+			//else {
+			//	fc << c << " ";
+			//}
+		}
+		fc << c << " ";
+	}
+	else if(!fb) {
+		while (fa) {
+			while (a < b) {
+				//if (c < a) {
+				//	fc << c << " " << a << " ";
+				//	c = INT_MAX;
+				//	a = b;
+				//	fa >> b;
+				//}
+				fc << a << " ";
+				a = b;
+				fa >> b;
+			}
+			fc << a << " ";
+			a = b;
+			fa >> b;
+			//if (c != INT_MAX) {
+			//	if (a < c) {
+			//		fc << a << " " << c << " ";
+			//	}
+			//	else {
+			//		fc << c << " " << a << " ";
+			//	}
+			//}
+			//else {
+			//	fc << a << " ";
+			//}
+		}
+		fc << a << " ";
+	}
+	fa.close();
+	fb.close();
+	fc.close();
+	fd.close();
+	std::ifstream res(name4);
+	if (res.peek() == EOF) {
+		res.close();
+		return 1;
+	}
+	else { 
+		res.close();
+		return -1; }
 }
 
 int main() {
@@ -106,7 +212,7 @@ int main() {
 	puts("");
 
 	std::string name = "f.txt";
-	random_file_gen(name, size, lim);
+	random_file_gen(name, lim, size);
 	std::fstream f(name);
 	if (!f) return -1;
 
@@ -129,74 +235,38 @@ int main() {
 	//firts splitting
 	int a, b;
 	f >> a;
+	if (!f) return -1;
 	f >> b;
-	int count = 1, k = 0;
 	while (f) {
-		if (a < b && count == 1) {
-			fa << a << " ";
-			a = b;
-			f >> b;
+		fa << a << " ";
+		if (a > b) {
+			fa.swap(fb);
 		}
-		else if (a < b && count == 2) {
-			fb << a << " ";
-			a = b;
-			f >> b;
-		}
-		else if (a >= b && count == 1) {
-			fa << a << " " << INT_MAX << " ";
-			a = b;
-			f >> b;
-			count = 2;
-			k += 1;
-		}
-		else if (a >= b && count == 2) {
-			fb << a << " " << INT_MAX << " ";
-			a = b;
-			f >> b;
-			count = 1;
-			k += 1;
-		}
+		a = b;
+		f >> b;
 	}
-	if (count == 1) {
-		fa << b << " " << INT_MAX;
-		k += 1;
-	}
-	else if (count == 2) {
-		fb << b << " " << INT_MAX;
-		k += 1;
-	}
+	fa << a << " ";
+	f.close();
+	fa.close();
+	fb.close();
+
 
 	std::ofstream f3;
 	f3.open("fc.txt");
 	if (!f3) return -4;
 	f3.close();
-	std::fstream fc;
-	fc.open("fc.txt");
 	std::string name3 = "fc.txt";
 
 	std::ofstream f4;
 	f4.open("fd.txt");
 	if (!f4) return -5;
 	f4.close();
-	std::fstream fd;
-	fd.open("fd.txt");
 	std::string name4 = "fd.txt";
 
 	//other splittings
-	bool res = false;
-	while(!res){
-		fa.open(name1);
-		fb.open(name2);
-		fc.open(name3);
-		fd.open(name4);
-		res = file_splitting(name1, name2, name3, name4, k);
-		trade(name1, name3);
-		trade(name2, name4);
-	}
+	file_splitting(name1, name2, name3, name4);
+	//trade(name1, name3);
+	//trade(name2, name4);
+	/*file_splitting(name3, name4, name1, name2);*/
 
-	f.close();
-	fa.close();
-	fb.close();
-	fc.close();
-	fd.close();
 }
